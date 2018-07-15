b4984e57e4f8283c1f3396a5ce96fc2e89b3852e
lym84180790 <84180790@qq.com>
2018-07-15 15:38:25 +0800

    消息总线(Spring Cloud Bus)

---
+++ Chapter08/lym-ha-config-client/src/main/java/com/lym/service/a/entity/User.java
@@ -0,0 +1,56 @@
+package com.lym.service.a.entity;
+
+import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+public class User {
+
+	private String id;
+	private String name;
+	private Integer age;
+	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+	private Date birthday;
+	
+	private String userId;
+	private String userName;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getAge() {
+		return age;
+	}
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	public Date getBirthday() {
+		return birthday;
+	}
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	
+	
+}
